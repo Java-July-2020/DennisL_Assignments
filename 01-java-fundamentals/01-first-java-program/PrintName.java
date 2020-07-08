5b309cb06aa219b0cd7a071b691c94cb2976db6b
Dennis Lee <leedennis04@gmail.com>
2020-07-07 21:03:29 -0500

    completed first java program

---
+++ 01-java-fundamentals/01-first-java-program/PrintName.java
@@ -0,0 +1,7 @@
+public class PrintName{
+    public static void main(String[] args){
+        System.out.println("My name is Dennis Lee");
+        System.out.println("I am 30 years old");
+        System.out.println("My hometown is Manitowak, WI");
+    }
+}
