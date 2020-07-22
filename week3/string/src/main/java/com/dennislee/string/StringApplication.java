91d6b73b22d6a50ca48ab297d4b920aad3151268
Dennis Lee <leedennis04@gmail.com>
2020-07-22 13:50:10 -0500

    after pull

--- week3/string/src/main/java/com/dennislee/string/StringApplication.java
+++ week3/string/src/main/java/com/dennislee/string/StringApplication.java
@@ -2,12 +2,20 @@ package com.dennislee.string;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;on.*;
 
 @SpringBootApplication
+@RestController
 public class StringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StringApplication.class, args);
 	}
 
+	@RequestMapping("/")
+	public String hello() {
+		System.out.println("Hello Client! How are you doing?");
+	}
+	
 }
