64b01da99ed6d29b59231a22ff0d1ac0b1eb27b0
Dennis Lee <leedennis04@gmail.com>
2020-07-31 11:40:02 -0500

    built the basics for the update and delete assignment

---
+++ 03-java-spring/02-spring-data-i/01-update-and-delete/src/main/java/com/dennislee/update/repositories/BookRepository.java
@@ -0,0 +1,15 @@
+package com.dennislee.update.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dennislee.update.models.Book;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, Long>{
+
+	 // this method retrieves all the books from the database
+	 List<Book> findAll();
+}
