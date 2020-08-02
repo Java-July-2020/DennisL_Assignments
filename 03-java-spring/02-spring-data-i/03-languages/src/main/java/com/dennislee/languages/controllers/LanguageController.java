f5ddc503e2fa980d7fc8441d35ee6cbfe8fd4fdb
Dennis Lee <leedennis04@gmail.com>
2020-08-02 17:04:35 -0500

    started the basic foundations of the langauges assignment

---
+++ 03-java-spring/02-spring-data-i/03-languages/src/main/java/com/dennislee/languages/controllers/LanguageController.java
@@ -0,0 +1,14 @@
+package com.dennislee.languages.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LanguageController {
+
+	@RequestMapping("/languages")
+	public String languages() {
+		return "index.jsp";
+	}
+
+}
---
+++ 03-java-spring/02-spring-data-i/03-languages/src/main/java/com/dennislee/languages/models/Language.java
@@ -0,0 +1,119 @@
+package com.dennislee.languages.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="language")
+public class Language {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	@Size(min = 2, max = 20)
+	private String name;
+	
+	@Column
+	@Size(min = 2, max = 20)
+	private String creator;
+	
+	@Column
+	@NotNull
+	private double currentVersion;
+	
+    // This will not allow the createdAt column to be updated after creation
+    @Column(updatable=false)
+    @DateTimeFormat(pattern = "MM/dd/yyyy HH:mm:ss")
+    private Date createdAt;
+    
+    @DateTimeFormat(pattern = "MM/dd/yyyy HH:mm:ss")
+	private Date updatedAt;
+    
+    // This will will auto populate the current date, for the object's created date field, prior to the object being created
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+    
+ // This will will auto populate the current date, for the object's updated date field, prior to the object updated,
+ // This will also updated the language currentVersion by 0.1 for every time it is updated
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+        this.currentVersion = this.currentVersion + 0.1;
+    }
+
+	public Language() {
+		this.currentVersion = 1.0;
+	}
+
+	public Language(@Size(min = 2, max = 20) String name, @Size(min = 2, max = 20) String creator) {
+		super();
+		this.name = name;
+		this.creator = creator;
+		this.currentVersion = 1.0;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCreator() {
+		return creator;
+	}
+
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+
+	public double getCurrentVersion() {
+		return currentVersion;
+	}
+
+	public void setCurrentVersion(double currentVersion) {
+		this.currentVersion = currentVersion;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+}
